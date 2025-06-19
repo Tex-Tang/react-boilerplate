f324be0c26976a1d49a5b87031ea39b9ae7e97cf
Tex-Tang <tex_tang@outlook.com>
2025-06-20 00:32:30 +0800

    Update directory

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,6 +1,6 @@
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
-import type { paths } from "./types/api";
+import type { paths } from "./schema.d.ts";
 
 const fetchClient = createFetchClient<paths>({
   baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
